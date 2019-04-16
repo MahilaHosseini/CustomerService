e533340057546d8cb1de342477ba22c91d6358a7
mahila <mahila.hosseini@yahoo.com>
2019-04-16 23:53:33 +0430

    Activiti initial config

--- src/main/java/com/customerService/app/config/JpaAuditingConfiguration.java
+++ src/main/java/com/customerService/app/config/JpaAuditingConfiguration.java
@@ -1,4 +1,4 @@
-package com.customerService.app;
+package com.customerService.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
